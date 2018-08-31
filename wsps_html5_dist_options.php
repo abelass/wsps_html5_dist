8fe8ef16744ee8074e9a7ec8923dd8625d3d7477
abelass <rainer@websimple.be>
2018-08-31 16:00:15 +0200

    adaptation de Z5 depuis la zone

--- wsps_html5_dist_options.php
+++ wsps_html5_dist_options.php
@@ -1,6 +1,6 @@
 <?php
 if (!isset($GLOBALS['z_blocs']))
-	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
+	$GLOBALS['z_blocs'] = array('content','extra1','extra2','head','head_js','header','footer');
 
 define('_ZENGARDEN_FILTRE_THEMES','spipr');
 define('_ALBUMS_INSERT_HEAD_CSS',false);
@@ -10,9 +10,8 @@ if (
 		_SPIPR_AUTH_DEMO
 		:
 		(isset($GLOBALS['visiteur_session']['statut'])
-    AND $GLOBALS['visiteur_session']['statut']=='0minirezo'
-    AND $GLOBALS['visiteur_session']['webmestre']=='oui')
+		AND $GLOBALS['visiteur_session']['statut']=='0minirezo'
+		AND $GLOBALS['visiteur_session']['webmestre']=='oui')
 	)
 	_chemin(_DIR_PLUGIN_SPIPR_DIST."demo/");
 
-?>
