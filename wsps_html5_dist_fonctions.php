2741d0dd2b0567407ded4bf4c7cbd967c0a3dc0b
Rainer Müller <rainer@websimple.be>
2019-07-09 10:40:31 +0200

    responsive menu

--- wsps_html5_dist_fonctions.php
+++ wsps_html5_dist_fonctions.php
@@ -1,43 +1,5 @@
 <?php
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
-/**
- * Ajouter le markup html pour une navbar responsive
- * [<div class="navbar navbar-inverse navbar-responsive" id="nav">
- * (#INCLURE{fond=inclure/nav,env}|navbar_responsive)
- * </div>]
- *
- * @param string $nav
- * @param string $class_collapse nom de la class à plier/déplier
- * @return string
- */
-function navbar_responsive($nav, $class_collapse = 'nav-collapse-main'){
-	if (strpos($nav,'nav-collapse')!==false) return $nav;
-
-	$respnav = '';
-
-	$uls = extraire_balises($nav,"ul");
-	$n = 1;
-	while ($ul = array_shift($uls)
-		AND strpos(extraire_attribut($ul,"class"),"nav")===false){
-		$n++;
-	}
-	if ($ul){
-		$respnav = $nav;
-		$p = strpos($respnav,$ul);
-		$respnav = substr_replace($respnav,
-			'<a class="btn btn-navbar" data-toggle="collapse" data-target=".' . $class_collapse . '">' .
-				'<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a>' .
-				"\n".'<div class="nav-collapse ' . $class_collapse . ' collapse">',$p,0);
-		$l=strlen($respnav);$p=$l-1;
-		while ($n--){
-			$p = strrpos($respnav,"</ul>",$p-$l);
-		}
-		if ($p)
-			$respnav = substr_replace($respnav,'</div>',$p+5,0);
-		else
-			$respnav = $nav;
-	}
-	return $respnav;
-}
+//define('_RESPONSIVENAV_OPENPOS', 'absolute');
--- wsps_html5_dist_options.php
+++ wsps_html5_dist_options.php
@@ -1,4 +1,6 @@
 <?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
 if (!isset($GLOBALS['z_blocs']))
 	$GLOBALS['z_blocs'] = array('content','extra1','extra2','head','head_js','header','footer');
 
