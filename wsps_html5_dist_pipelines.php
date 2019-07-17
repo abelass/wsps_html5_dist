7a4127e2cba9bad949a5feec7596e3a94f219355
Rainer Müller <rainer@websimple.be>
2019-07-17 09:44:48 +0200

    comment

--- wsps_html5_dist_pipelines.php
+++ wsps_html5_dist_pipelines.php
@@ -13,8 +13,19 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+/**
+ * complète ou modifierle résultat de la compilation d’un squelette donné.
+ *
+ * @param array $flux
+ *   Les données du pipeline
+ *
+ * @return array
+ *   Les données du pipeline
+ */
+
 function wsps_html5_dist_recuperer_fond($flux){
 
+	// Ajoute le bout de code pour le menu respionsive
 	if ($flux['args']['fond'] == 'inclure/nav'){
 		$menu_repsonsive  = recuperer_fond('inclure/menu_responsive');
 		$flux['data']['texte'] = preg_replace('|<ul>|', $menu_repsonsive . '<ul id="nav-menu">', $flux['data']['texte'], 1);
